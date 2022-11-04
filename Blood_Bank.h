b2598959ee5451632b9a8fc3e7f9a3506a034a12
Sachin Jayadev <93666921+Sachin5679@users.noreply.github.com>
2022-11-04 19:54:54 +0530

    Update Blood_Bank.h

--- Blood_Bank.h
+++ Blood_Bank.h
@@ -19,13 +19,15 @@ class BloodBank {
 void BloodBank::bloodBank(string s) {
     static int i = 0;
     if (s == "Add Donor") {
-        string n;
+        string n,m;
         dc[i].setDonorID();
 
+        
         cout<<"Enter name: ";
         getchar();
-        getline(cin>>n,n);
-        dc[i].setOwnerName(n);
+        getline(cin>>n,m);
+        dc[i].setOwnerName(n+" "+m);
+
 
         cout<<"Enter donation date: ";
         cin>>n;
@@ -38,8 +40,16 @@ void BloodBank::bloodBank(string s) {
         cout<<"Enter Blood Group: ";
         cin>>n;
         dc[i].setBloodGroup(n);
-        i++;
+        
+        cout<<"Enter Phone number: ";
+        cin>>n;
+        dc[i].setPhone(n);
 
+        cout<<"Enter Email-ID: ";
+        cin>>n;
+        dc[i].setEmail(n);
+
+        i++;
         cout<<"Donor details added successfully!";
         system("cls");
     }
@@ -53,9 +63,14 @@ void BloodBank::bloodBank(string s) {
             cout<<"Donation date: "<<dc[j].getDonationDate()<<endl;
             cout<<"Aadhar number: "<<dc[j].getAadhar()<<endl;
             cout<<"Blood group: "<<dc[j].getBloodGroup()<<endl<<endl;
-            system("pause");
-            system("cls");
+            cout<<"Phone number: "<<dc[j].getPhone()<<endl;
+            cout<<"Email-ID: "<<dc[j].getEmail()<<endl;
+            cout<<"-----------------------------"<<endl;
+            // system("pause");
+            // system("cls");
         }
+        system("pause");
+        system("cls");
         if(j<1)
             cout<<"Directory is empty!";
     }
@@ -71,14 +86,54 @@ void BloodBank::bloodBank(string s) {
                 cout<<"Donor name: "<<dc[j].getOwnerName()<<endl;
                 cout<<"Donation date: "<<dc[j].getDonationDate()<<endl;
                 cout<<"Aadhar number: "<<dc[j].getAadhar()<<endl;
-                cout<<"Blood group: "<<dc[j].getBloodGroup()<<endl<<endl;
+                cout<<"Blood group: "<<dc[j].getBloodGroup()<<endl;
+                cout<<"Phone number: "<<dc[j].getPhone()<<endl;
+                cout<<"Email-ID: "<<dc[j].getEmail()<<endl<<endl;
                 system("pause");
                 system("cls");
             }
-            else {
+            /*else {
                 cout<<"Not found."<<endl;
-            }
+            }*/
         }
+    }
+
+    else if (s=="Update Donor Details") {
+        string upd;
+        int ch;
+        system("cls");
+        cout<<"Enter Donor ID to update details"<<endl;
+        cin>>upd;
+        for (int j=0; j<i; j++)
+        {
+            if (upd==dc[j].getDonorID()) {
+                cout<<"Which detail do you want to update?"<<endl;
+                cout<<"1-Phone number"<<endl;
+                cout<<"2-Email ID"<<endl;
+                cout<<"Enter choice: ";
+                cin>>ch;
+                if (ch==1)
+                {
+                    string newPhn;
+                    cout<<"Enter new phone number: ";
+                    cin>>newPhn;
+                    dc[j].setPhone(newPhn);
+                    cout<<"New Phone number: "<<dc[j].getPhone()<<endl;
+                }
+                else if (ch==2)
+                {
+                    string newEmail;
+                    cout<<"Enter new email: ";
+                    cin>>newEmail;
+                    dc[j].setEmail(newEmail);
+                    cout<<"New Email-ID: "<<dc[j].getEmail()<<endl;
+                }
+            }
+            /*else {
+                cout<<"No entry with this ID exists."<<endl;
+            }*/
+        }
+
     }
 }
 
